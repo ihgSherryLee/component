9e5dd51ae6fe4e1c965d06db682afe212b537c0d
“ihgSherryLee” <lxyihggyml@163.com>
2019-08-08 18:25:41 +0800

    test

--- src/views/waterMark.vue
+++ src/views/waterMark.vue
@@ -17,12 +17,12 @@ export default class WaterMarkVue extends Vue {
   public waterMark: any;
   public mounted() {
     const waterMark = new WaterMark(['abc', 'def', '2019-08-08']);
-    // waterMark.option = {
-    //   font: '28px 微软雅黑',
-    //   fillStyle: 'rgba(204, 204, 204, 0.5)',
-    //   canvasHeight: 80,
-    //   canvasWidth: 600,
-    // };
+    waterMark.option = {
+      font: '28px 微软雅黑',
+      fillStyle: 'rgba(204, 204, 204, 0.5)',
+      canvasHeight: 80,
+      canvasWidth: 600,
+    };
     waterMark.setMark();
     this.waterMark = waterMark;
   }
